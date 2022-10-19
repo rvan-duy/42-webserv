6f168e28fdc3d362b9f857fec51c1904bf0b59bb
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2022-10-19 12:42:07 +0200

    Added include for ubuntu compile

--- include/Socket.hpp
+++ include/Socket.hpp
@@ -9,6 +9,7 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <cstring>
 
 /****************************************************************************************************/
 /* The Socket Class                                                                                 */
