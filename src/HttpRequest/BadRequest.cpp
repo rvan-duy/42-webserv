40a4aa6534fb7dd5288d9aacef541e3e7645f638
Oswin Bult <obult@student.codam.nl>
2022-12-02 18:21:42 +0100

    voided some vars in badrequest'

--- src/HttpRequest/BadRequest.cpp
+++ src/HttpRequest/BadRequest.cpp
@@ -9,9 +9,12 @@ BadRequest::BadRequest(const BadRequest& ref) : HttpRequest(ref)  {}
 BadRequest::~BadRequest() {}
 
 int BadRequest::executeRequest(Server& server) {
+  (void)server;
   return (0);
 }
 
 HttpResponse BadRequest::constructResponse(Server& server, std::string& index) {
+  (void)server;
+  (void)index;
   return HttpResponse(HTTP_1_1, 406, "Bad Request");
 }
