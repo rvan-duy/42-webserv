f55edebc71d4063368eefa5c268c16f0166537cd
Oswin Bult <obult@student.codam.nl>
2022-12-02 18:18:59 +0100

    bad request interface added

---
+++ src/HttpRequest/BadRequest.cpp
@@ -0,0 +1,17 @@
+#include "HttpRequest.hpp"
+
+#include <cstdio>
+
+BadRequest::BadRequest(std::string& msg) : HttpRequest(msg) {}
+
+BadRequest::BadRequest(const BadRequest& ref) : HttpRequest(ref)  {}
+
+BadRequest::~BadRequest() {}
+
+int BadRequest::executeRequest(Server& server) {
+  return (0);
+}
+
+HttpResponse BadRequest::constructResponse(Server& server, std::string& index) {
+  return HttpResponse(HTTP_1_1, 406, "Bad Request");
+}
