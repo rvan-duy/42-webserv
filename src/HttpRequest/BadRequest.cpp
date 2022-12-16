6d41010bd5076e0534bb161c74d4974ab9e6f2c5
Rutger Cappendijk <rutgercappendijk@gmail.com>
2022-12-16 12:46:01 +0100

    removed some comments

--- src/HttpRequest/BadRequest.cpp
+++ src/HttpRequest/BadRequest.cpp
@@ -17,21 +17,6 @@ HttpResponse BadRequest::constructResponse(Server &server, std::string &index)
 {
     (void)server;
     (void)index;
-    std::string message = getErrorMessage();
+    std::string message = getMessageByStatusCode(_statusCode);
     return HttpResponse(HTTP_1_1, _statusCode, message);
-}
-
-std::string BadRequest::getErrorMessage() const
-{
-    switch (_statusCode)
-    {
-    case INTERNAL_SERVER_ERROR:
-        return "Internal server error";
-    case METHOD_NOT_ALLOWED:
-        return "Method not allowed";
-    case CONTENT_TOO_LARGE:
-        return "Content too lage";
-    default:
-        return "Bad request";
-    }
 }
--- src/HttpRequest/GetRequest.cpp
+++ src/HttpRequest/GetRequest.cpp
@@ -12,46 +12,31 @@ int GetRequest::executeRequest(Server &server)
   return 0;
 }
 
-/**
- * Define a directory or a file from where the file should be searched (for
-example, if url /kapouet is rooted to /tmp/www, url /kapouet/pouic/toto/pouet is /tmp/www/pouic/toto/pouet).
-*/
-// TODO: Make route getting great again
-// static Route getCorrectRoute(Server const &server, std::string _url)
-// {
-//   std::vector<Route> routes = server.getRoutes();
-
-//   for (std::vector<Route>::iterator it = routes.begin(); it != routes.end(); it++)
-//   {
-//     if (it->route == _url)
-//     {
-//       return *it;
-//     }
-//   }
-//   // TODO: If no route matches -> return default route
-//   return Route();
-// }
-
-HttpResponse GetRequest::constructResponse(Server& server, std::string& index) {
+HttpResponse GetRequest::constructResponse(Server &server, std::string &index)
+{
   Logger &logger = Logger::getInstance();
   logger.log("Response type: GET");
   (void)server;
   HttpResponse response;
 
-  std::string path = "root" + getUrl();  // future get root from server class
+  std::string path = "root" + getUrl(); // future get root from server class
   logger.log("Path: " + path);
 
-  if (!_fileExists(path)) {
+  if (!_fileExists(path))
+  {
     logger.log("File doesn't exist");
 
     const std::string index_path = path + index;
     logger.log("Looking for index path: " + index_path);
-    if (!_fileExists(index_path)) {
+    if (!_fileExists(index_path))
+    {
       logger.log("Index file doesn't exist");
 
       response._setResponse("root/404/index.html", 404, "Not Found", getVersion()); // very hardcoded lol
       return response;
-    } else {
+    }
+    else
+    {
       logger.log("Index file exists");
       path = index_path;
     }
