d2b6998ec26d341a55f34ebcb56b50a923b5139c
Rutger Cappendijk <rutgercappendijk@gmail.com>
2022-12-19 12:36:58 +0100

    bug fixed

--- src/socket/processingRequests.cpp
+++ src/socket/processingRequests.cpp
@@ -47,17 +47,16 @@ static std::string getHostWithoutPort(HttpRequest *request)
 }
 
 /* Checks which servers are listening to the host from the request*/
-static int matchBasedOnHost(Server *dest, std::vector<Server> &allServers, std::string const &host)
+static Server &matchBasedOnHost(std::vector<Server> &allServers, std::string const &host)
 {
     for (std::vector<Server>::iterator it = allServers.begin(); it != allServers.end(); ++it)
     {
         if (it->getServerName() == host)
         {
-            *dest = *it;
-            return 0;
+            return *it;
         }
     }
-    return 1;
+    throw std::runtime_error("No matching server found");
 }
 
 /**
@@ -72,13 +71,13 @@ void Socket::_matchRequestToServer(int const &clientFd, HttpRequest *request)
         delete request;
         return _addBadRequestToClient(clientFd, HTTPStatusCode::BAD_REQUEST);
     }
-    Server result;
-    if (matchBasedOnHost(&result, _servers, hostWithoutPort))
-    {
+    try {
+        Server& result = matchBasedOnHost(_servers, hostWithoutPort);
+        return _addRequestToClient(clientFd, request, &result);
+    } catch (std::runtime_error error) {
         delete request;
         return _addBadRequestToClient(clientFd, HTTPStatusCode::BAD_REQUEST);
     }
-    return _addRequestToClient(clientFd, request, &result);
 }
 
 int Socket::processRequest(int const &clientFd)
