26807634b54e251a20211206d3a3443a9dc402b7
Rutger Cappendijk <rutgercappendijk@gmail.com>
2022-12-20 13:31:16 +0100

    reverted main

--- src/main.cpp
+++ src/main.cpp
@@ -17,33 +17,14 @@ int startWebserver(std::vector<Socket> sockets) {
   return 0;
 }
 
-void testCgi(char **env) {
-  CGI testCgi(env);
-  std::string dest;
-  std::vector<std::string> headers;
-
-  HTTPStatusCode ret =
-      testCgi.executeFile(&dest, &headers, "test/cgi/helloworld.py", "yes");
-  if (ret == HTTPStatusCode::OK) {
-    std::cout << dest << std::endl;
-    std::cout << "----" << std::endl;
-    for (size_t i = 0; i < headers.size(); i++) {
-      std::cout << headers[i] << std::endl;
-    }
-  } else {
-    std::cout << getMessageByStatusCode(ret) << std::endl;
-  }
-}
-
 int main(int argc, char **argv, char **env) {
   (void)env;
   std::vector<Socket> sockets;
   /*  Input check */
-  testCgi(env);
-  // if (argc != 2) {
-  //   std::cout << "Usage: " << argv[0] << " <config_file>" << std::endl;
-  //   return 1;
-  // }
-  // if (initWebserver(&sockets, argv[1])) return 1;
-  // return startWebserver(sockets);
+  if (argc != 2) {
+    std::cout << "Usage: " << argv[0] << " <config_file>" << std::endl;
+    return 1;
+  }
+  if (initWebserver(&sockets, argv[1])) return 1;
+  return startWebserver(sockets);
 }
