ba7672ebcb713c4163d5ba07378de10982f357ad
Oswin Bult <obult@student.codam.nl>
2022-12-22 16:34:58 +0100

    added comment about tr: Illegal byte sequence to curler script

--- curler.sh
+++ curler.sh
@@ -10,6 +10,9 @@ BASE_URL=http://localhost:8083
 for i in $(seq 1 $REQUESTS)
 do
   # Generate a random string
+  # tr may complain about getting an Illegal byte sequence, this is not our webservs fault
+  # the reason is that tr does not recognice the /dev/urandom outbut as utf-8.
+  # on linux tr should work on all binary but on mac it does not or something like that. not my issue.
   RANDOM_STRING=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
   # Append the random string to the base URL
